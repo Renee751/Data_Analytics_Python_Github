0f072c437f5739d88321c6c5f710062e66593a86
Renee751 <132562241+Renee751@users.noreply.github.com>
2023-05-24 14:28:29 +0200

    start statements

---
+++ Module2-Functions-loops-conditionals/statements/main.py
@@ -0,0 +1,37 @@
+# Do not modify these lines
+__winc_id__ = "7599944cfbd94b47beffdbab7a208931"
+__human_name__ = "statements"
+
+# Add your code after this line
+
+one = 5 % (2 + 3) == 0
+two = ("Piano"[0:0] == ("" + "Guitar")[0:0])
+three = 2**(3 + 3) - (2 * 11) == 42
+
+# print(one,two,three)
+
+
+  
+def eligible_to_vote(age, nationality):
+    if age >= 18 and nationality == 'Italian':
+        print('Bene!')
+        return True
+    elif age >= 25 and nationality == 'Portuguese':
+        print('Sim!')
+        return True
+    elif age >= 31 and nationality == 'Dutch':
+        print('Top!')
+        return True
+    elif age >= 19 and nationality == 'Malawian':
+        print('Iya!')
+        return True
+    else:
+        return False
+
+def eligible_to_vote(age, nationality):
+    if age >= 18 and nationality == 'Italian':
+        return True
+    elif age >= 25 and nationality == 'Portuguese':
+        return True
+    else:
+        return False
